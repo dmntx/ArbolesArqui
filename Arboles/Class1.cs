bb0eb25b777c86a6b72226497e99bae1b47b3422
Juan Bernal Martinez <Juan Bernal Martinez@DESKTOP-SLA3921>
2023-01-17 21:47:57 -0600

    Agregar archivos de proyecto.

---
+++ Arboles/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arboles
+{
+    class Nodo
+    {
+        public string Nombre { get; set; }
+
+        public string Izquierda { get; set; }
+
+        public string Derecha { get; set; }
+    }
+}
