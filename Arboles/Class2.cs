5be0b4fdaadb679f45f65e0cd699c62d74b489a1
JUAN BERNAL MARTINEZ <juan.bernalm@alumno.buap.mx>
2023-01-17 23:11:46 -0600

    Termino de primera

---
+++ Arboles/Class2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arboles
+{
+    class Arbol
+    {
+        private Nodo raiz;
+        private Nodo trabajo;
+        private int i = 0;
+        public Arbol()
+        {
+            raiz = null;
+        }
+        internal Nodo Raiz { get => raiz; set => raiz = value; }
+
+        public Nodo Insertar(string dat, Nodo NodoN)
+        {
+            Nodo temp = null;
+            if(NodoN == null)
+            {
+                temp = new Nodo();
+                temp.Dato = dat;
+                return temp;
+            }
+            if(dat != NodoN.Dato)
+            {
+                NodoN.Izquierda = Insertar(dat, NodoN.Izquierda);
+            }
+            if(dat == NodoN.Dato) {
+                NodoN.Derecha = Insertar(dat, NodoN.Derecha);
+            }
+            return NodoN;
+        }
+        public void Transversa(Nodo NodoN)
+        {
+            if(NodoN == null)
+            {
+                return;
+            }
+            for(int n= 0; n < i; n++)
+            {
+                Console.WriteLine("");
+            }
+            Console.WriteLine(NodoN.Dato);
+
+            if(NodoN.Izquierda != null)
+            {
+                i++;
+                Console.Write("I ");
+                Transversa(NodoN.Izquierda);
+                i--;
+            }
+        }
+    }
+}
--- Arboles/Program.cs
+++ Arboles/Program.cs
@@ -6,7 +6,13 @@ namespace Arboles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Arbol arbol = new Arbol();
+            Nodo raiz = arbol.Insertar("5", null);
+            arbol.Insertar("2", raiz);
+            arbol.Insertar("e", raiz);
+            arbol.Insertar("g", raiz);
+            arbol.Insertar("3", raiz);
+            arbol.Transversa(raiz);
         }
     }
 }
