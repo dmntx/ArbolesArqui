73d4d2a8b236b208861c28dc678897c97c92a060
JUAN BERNAL MARTINEZ <juan.bernalm@alumno.buap.mx>
2023-01-19 20:58:05 -0600

    Comentarios de la clase arbol

--- Arboles/Class2.cs
+++ Arboles/Class2.cs
@@ -6,8 +6,8 @@ namespace Arboles
 {
     class Arbol
     {
-        private Nodo raiz;
-        private Nodo trabajo;
+        private Nodo raiz; //nodo raiz para inicializar el arbol
+        private Nodo trabajo; //
         private int i = 0;
         public Arbol()
         {
@@ -17,23 +17,23 @@ namespace Arboles
 
         public Nodo Insertar(string dat, Nodo NodoN)
         {
-            Nodo temp = null;
-            if(NodoN == null)
+            Nodo temp = null; //nodo auxiliar
+            if(NodoN == null) //Si el nodo que le pasamos aún no se inicializa
             {
-                temp = new Nodo();
-                temp.Dato = dat;
-                return temp;
+                temp = new Nodo(); //inicializamos el nodo temporal
+                temp.Dato = dat; //le asignamos el dato que queremos agregar al nodo temporal
+                return temp; //retornamos el nodo temporal
             }
-            if(dat != NodoN.Dato)
+            if(dat != NodoN.Dato) //aquí voy a corregir para el tipo de operacion que se necesite xd
             {
                 NodoN.Izquierda = Insertar(dat, NodoN.Izquierda);
             }
-            if(dat == NodoN.Dato) {
+            if(dat == NodoN.Dato) { //aqui igual, pero es incersion para los hijos del nodo
                 NodoN.Derecha = Insertar(dat, NodoN.Derecha);
             }
-            return NodoN;
+            return NodoN; //retornamos el nodoN si es que llegó a ser un hijo
         }
-        public void Transversa(Nodo NodoN)
+        public void Transversa(Nodo NodoN) //Es solo para verificar los hijos del arbol
         {
             if(NodoN == null)
             {
