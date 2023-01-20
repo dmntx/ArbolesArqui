bf8651569d2cd3f7bc2cd3ca20e40d6d025e4964
JUAN BERNAL MARTINEZ <juan.bernalm@alumno.buap.mx>
2023-01-19 20:43:33 -0600

    Comentarios clase nodo

--- Arboles/Class1.cs
+++ Arboles/Class1.cs
@@ -10,14 +10,14 @@ namespace Arboles
         private Nodo izq;
         private Nodo der;
 
-        public string Dato { get => dato; set=> dato = value; }
+        public string Dato { get => dato; set=> dato = value; } //metodos set y get para los nodos
         public Nodo Izquierda { get =>izq; set=> izq = value; }
 
         public Nodo Derecha { get => der; set => der = value; }
         public Nodo()
         {
-            dato = "0";
-            izq = null;
+            dato = "";
+            izq = null; //inicializamos el constructor del nodo con los datos en null
             der = null;
             }
     }
